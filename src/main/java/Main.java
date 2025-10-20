2d9ef2edc00583c88806d4f4a3845dcf59e339f9
Jonatan Rodríguez <jonodomongos@hotmail.com>
2025-10-20 14:00:45 +0200

    commit inicial

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+    /*
+    INSTRUCCIONES:
+    CREAR TABLA PELICULAS
+    Crea una tabla con al menos 10 películas con los siguientes campos:
+        - id autoincrementable
+        - titulo pelicula
+        - protagonista
+        - año
+        - género
+        - recaudación
+
+        Primero, prueba a hacer consultas con los botoncitos: select, update, delete, insert...
+        Después haz consultas del tipo:
+            - Ver solo titulo y año
+            - Todas las películas producidas a partir de cierto año, en orden ascendente o descendente
+            - Ordenar por titulo
+            - Agrupar por género
+            - Ver solo las pelis con recaudación menor que X
+            - Ver películas entre un año y otro de producción
+            - Ver cuantas pelis hay en la lista
+            - Ver la suma de todas las recaudaciones
+            - Ver la media de las recaudaciones
+            - Acuérdate de renombrar las columnas cuando creas necesario (AS)
+    */
+    }
+}
